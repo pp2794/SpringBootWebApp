281e163569f67c74ec09126b0ee94a4d90e0a806
pp2794 <pp2794>
2019-10-01 15:50:32 +0530

    Continued Basic development.
    1. Added post, delete, update operations
    2. Added DB dependencies.

--- springBootWebApp/src/main/java/com/piy/springBootWebApp/controller/ShipwreckController.java
+++ springBootWebApp/src/main/java/com/piy/springBootWebApp/controller/ShipwreckController.java
@@ -2,6 +2,8 @@ package com.piy.springBootWebApp.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,35 @@ import com.piy.springBootWebApp.model.Shipwreck;
 @RestController
 @RequestMapping("/api/v1")
 public class ShipwreckController {
-
+	
 	@RequestMapping(value="shipwrecks", method=RequestMethod.GET)
-	List<Shipwreck> list(){
+	public List<Shipwreck> list(){
 		return ShipwreckStub.list();
 	}
+	
+	@RequestMapping(value="shipwrecks/{id}", method=RequestMethod.GET)
+	public Shipwreck get(@PathVariable long id) {
+		return ShipwreckStub.get(id);
+	}
+	
+	@RequestMapping(value="shipwrecks", method=RequestMethod.POST)
+	public Shipwreck create(@RequestBody Shipwreck wreck) {
+		return ShipwreckStub.create(wreck);
+	}
+	
+	@RequestMapping(value="shipwrecks/{id}", method=RequestMethod.PUT)
+	public Shipwreck update(@RequestBody Shipwreck wreck, @PathVariable long id) {
+		return ShipwreckStub.update(id, wreck);
+	}
+	
+	@RequestMapping(value="shipwrecks/{id}",method=RequestMethod.DELETE)
+	public Shipwreck delete(@PathVariable long id) {
+		return ShipwreckStub.delete(id);
+	}
+	
+	
+	
+	
+	
+	
 }
