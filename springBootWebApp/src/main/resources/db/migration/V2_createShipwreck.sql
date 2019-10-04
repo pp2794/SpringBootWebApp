6ebf4d8defbbe1c7ea35320095e68b410078c6b3
pp2794 <pp2794>
2019-10-04 16:39:45 +0530

    DB related changes.
    1. Added flywayDB dependency for DB versioning.
    2. Added hibernate dependencies.

---
+++ springBootWebApp/src/main/resources/db/migration/V2_createShipwreck.sql
@@ -0,0 +1,11 @@
+create table Shipwreck
+(
+id int auto_increment,
+name varchar(255),
+description varchar(2000),
+condition varchar(255),
+depth int,
+latitude double,
+longitude double,
+yearDiscovered int,
+);
